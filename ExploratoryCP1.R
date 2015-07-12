df676f43a8b2ca3302a2de8a4ce39c6462ea78ab
Rosa Alarcon <rosa.alarcon@gmail.com>
2015-07-12 18:24:40 -0300

    Exploratory Data Analysis Course Project 1, first commit

---
+++ ExploratoryCP1.R
@@ -0,0 +1,7 @@
+## Nrows = 2,075,259
+## Ncols = 9 (10 char, 8 char, num, num, num, num, num, num, num)
+## Nrows x (10Bytes + 8Bytes + 7x8Bytes) 
+##   = Nrows x 74Bytes 
+##   = 153.569.166Bytes
+##   = 153.569.166/2^20 = 146.4549694 MB
+##   = 146.5 MB
